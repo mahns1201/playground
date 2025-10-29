7008cb170a95172b247a1beb3b654d2c1774d988
서민혁 <mahns1201@gmail.com>
2025-10-30 01:41:51 +0900

    Feat junit5

--- java-11/build.gradle.kts
+++ java-11/build.gradle.kts
@@ -1,1 +1,1 @@
-plugins {    java}java {    toolchain {        languageVersion.set(JavaLanguageVersion.of(11))    }}repositories {    mavenCentral()}
+plugins {    java}java {    toolchain {        languageVersion.set(JavaLanguageVersion.of(11))    }}repositories {    mavenCentral()}dependencies {    testImplementation(platform("org.junit:junit-bom:5.11.3"))    testImplementation("org.junit.jupiter:junit-jupiter")}tasks.test {    useJUnitPlatform()}
